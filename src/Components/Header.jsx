eb9bdecb54c21d5759161097f95bacb2253f059c
jhoaking <peredoroj@gmail.com>
2024-01-17 12:46:11 -1100

    headerCss

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,36 +4,44 @@ import { FaInstagram } from "react-icons/fa6";
 const Header = () => {
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            <a href="">Inicio</a>
-          </li>
-          <li>
-            <a href="">Sobre Mi</a>
-          </li>
-          <li>
-            <a href="">Mis Proyectos</a>
-          </li>
-          <li>
-            <a href="">Contactame!</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="menu  container">
+        <div class="toggle-switch">
+          <label class="switch-label">
+            <input type="checkbox" class="checkbox" />
+            <span class="slider"></span>
+          </label>
+        </div>
+        <nav >
+          <ul>
+            <li>
+              <a href="">Inicio</a>
+            </li>
+            <li>
+              <a href="">Sobre Mi</a>
+            </li>
+            <li>
+              <a href="">Mis Proyectos</a>
+            </li>
+            <li>
+              <a href="">Contactame!</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
-      <div className="header-content">
+      <div className="header-content ">
         <div className="header-txt">
-          <h1>Bernardo Peredo</h1>
+          <h1>Desarollador Frontend React</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            labore exercitationem, enim voluptatem voluptates magni odit harum.
-            Neque fugit iure officia iusto ea, voluptatem est, quidem nisi
-            repudiandae, magni vero!
+            ¡Hola! Mi nombre es Bernardo Peredo, tengo 17 años y soy un
+            freelancer especializado en desarrollo web. Me apasiona aprender y
+            practicar programación todos los días para mejorar mis habilidades y
+            ofrecer servicios de alta calidad.
           </p>
           <div className="btns">
-            <button>Ver CV</button>
-            <FaWhatsapp />
-            <FaInstagram />
+            <button className="btn1">Ver CV</button>
+            <FaWhatsapp size={30} className="icons1" />
+            <FaInstagram size={30} className="icons2" />
           </div>
         </div>
         <div className="imgBernardo">
