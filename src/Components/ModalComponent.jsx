6a7178baf03e7c34d8c84f3ea3d37f3b7797e778
jhoaking <peredoroj@gmail.com>
2024-01-19 14:32:01 -1100

    dadadadafafA

--- src/Components/ModalComponent.jsx
+++ src/Components/ModalComponent.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import { usePorfolioContext } from '../Context/ContextPorfolio';
-import DataModal from '../Data/DataModal.json'
+import React from "react";
+import { usePorfolioContext } from "../Context/ContextPorfolio";
+import DataModal from "../Data/DataModal.json";
 
 const ModalComponent = ({ projectNumber }) => {
-  const { isDarkMode, theme, modalStates, handleProjectModal } = usePorfolioContext();
-  const currentProject = DataModal.find(project => project.id === projectNumber);
+  const { isDarkMode, theme, modalStates, handleProjectModal } =
+    usePorfolioContext();
+  const currentProject = DataModal.find(
+    (project) => project.id === projectNumber
+  );
 
   if (!currentProject) {
     // Si no se encuentra el proyecto, puedes manejarlo según tus necesidades
     return null;
   }
-
-  
 
   return (
     <div>
@@ -22,36 +23,48 @@ const ModalComponent = ({ projectNumber }) => {
             background: isDarkMode ? theme.backgroun2D : theme.backL2,
           }}
         >
-          <button className='close' onClick={() => handleProjectModal(projectNumber)}>X</button>
+          <button
+            className="close"
+            onClick={() => handleProjectModal(projectNumber)}
+          >
+            X
+          </button>
           <div className="megaCOntainer">
             <div className="modalContent">
-                <h1 style={{ color: isDarkMode ? theme.detallenos2D : theme.textD }}>
-                  {currentProject.title}
-                </h1>
+              <h1
+                style={{ color: isDarkMode ? theme.detallenos2D : theme.textD }}
+              >
+                {currentProject.title}
+              </h1>
 
-           <div className="boxTheInfoi">
-           <div className="infoModal">
-                <h1 style={{ color: isDarkMode ? theme.detallenos2D : theme.textD }}>
-                  Información:
-                </h1>
+              <div className="boxTheInfoi">
+                <div className="infoModal">
+                  <h1
+                    style={{
+                      color: isDarkMode ? theme.detallenos2D : theme.textD,
+                    }}
+                  >
+                    Información:
+                  </h1>
+                  <p style={{ color: isDarkMode ? theme.textD : theme.textL }}>
+                    {currentProject.content}
+                  </p>
+                </div>
+              </div>
+
+              <div className="Aprendizaje">
                 <p style={{ color: isDarkMode ? theme.textD : theme.textL }}>
-                  {currentProject.content}
+                  <h3
+                    style={{
+                      color: isDarkMode ? theme.detallenos2D : theme.textD,
+                    }}
+                  >
+                    Mi Aprendizaje
+                  </h3>
+                  :{currentProject.aprendizaje}
                 </p>
               </div>
             </div>
-
-            <div className="Aprendizaje">
-              <h1 style={{ color: isDarkMode ? theme.detallenos2D : theme.textD }}>
-                Mi Aprendizaje
-              </h1>
-              <p style={{ color: isDarkMode ? theme.textD : theme.textL }}>
-                {currentProject.aprendizaje}
-              </p>
-           
-
-            </div>
-           </div>
-
           </div>
         </div>
       )}
